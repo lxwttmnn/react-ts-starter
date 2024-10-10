09045039aca8330bd65cf1a6cea888818975eaff
Alexander Wittmann <lxwtmn@gmail.com>
2024-10-10 14:00:36 +0200

    fixed test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,6 +1,15 @@
 import { describe, expect, it } from 'vitest';
 import { render } from '@testing-library/react';
+import { vi } from 'vitest';
 import App from './App';
+
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn()
+}));
+
+vi.stubGlobal('ResizeObserver', ResizeObserverMock);
 
 describe('render', () => {
   it('renders the main page', () => {
